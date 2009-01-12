dd7960070f8e184a30dad7807e5e22e760866da7
p.aboyoun <p.aboyoun@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-01-12 06:21:14 +0000

    Adding quote and ignoreAllBasePackages arguments to writeNamespaceImports.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/codetoolsBioC@36559 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/writeNamespaceImports.R
+++ R/writeNamespaceImports.R
@@ -1,16 +1,29 @@
-writeNamespaceImports <- function(package, file = "", append = FALSE) {
+writeNamespaceImports <-
+function(package, file = "", append = FALSE, quote = FALSE, ignoreAllBasePackages = FALSE) {
     writeImports <- function(x, prefix, file) {
         spaces <- paste(rep(" ", nchar(prefix) + 1), collapse = "")
-        for (i in seq_len(length(x))[!(names(x) %in%
-                                       c("base", "Autoloads"))]) {
-            qstring <- rep("", length(x[[i]]))
-            qstring[grep("<-$", x[[i]])] <- "\""
-            cat(paste(prefix, "(", names(x[i]), ",\n", spaces,
-                      paste(qstring, x[[i]], qstring, sep = "",
-                      collapse = paste(",\n", spaces, sep = "")),
-                            ")", sep = ""), file = file)
+        for (i in seq_len(length(x))[!(names(x) %in% ignoredPackages)]) {
+            if (quote) {
+                qstring1 <- "\""
+                qstring2 <- "\""
+            } else {
+                qstring1 <- ""
+                qstring2 <- rep("", length(x[[i]]))
+                qstring2[grep("<-", x[[i]])] <- "\""
+            }
+            cat(paste(prefix, "(", qstring1, names(x[i]), qstring1, ",\n",
+                      spaces, paste(qstring2, x[[i]], qstring2, sep = "",
+                                    collapse = paste(",\n", spaces, sep = "")),
+                      ")", sep = ""), file = file)
             cat("\n\n", file = file)
         }
+    }
+    if (ignoreAllBasePackages) {
+        ignoredPackages <-
+          c("stats", "graphics", "grDevices", "utils", "datasets", "methods",
+            "base", "Autoloads")
+    } else {
+        ignoredPackages <- c("base", "Autoloads")
     }
     if (file == "") 
         file <- stdout()
