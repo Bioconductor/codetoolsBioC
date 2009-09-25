6373d2e68c96ca52eac39b69d582f3bd83ebed98
c.wong <c.wong@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-09-25 22:51:15 +0000

    Added getRdFileNames.R
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/codetoolsBioC@41970 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/getRdFileNames.R
@@ -0,0 +1,26 @@
+getRdFileNames <- function(topic, package=NULL)
+{
+   ## example 1: getRd("ExpressionSet", "Biobase")
+   ## example 2: getRd("pData")
+
+   if (missing(topic)) stop("'topic' must be given")
+   if (length(topic) > 1) 
+       stop("'topic' should be a length-one character vector. ")
+
+
+   topic <- as.character(topic)
+   lib.loc <- .libPaths()
+
+   if (!is.null(package) & !is.character(package))
+       stop("'package' should be a character vector.")
+     
+   if (is.null(package))
+       package <- .packages(all.available = TRUE, lib.loc = lib.loc)
+
+   paths <- sapply(.find.package(package, lib.loc),
+                  function(p) index.search(topic, p, "AnIndex"))
+   paths <- paths[paths != ""]
+   
+   return(data.frame(packages=basename(names(paths)), Rd.file=basename(paths)))
+ }
+ 
