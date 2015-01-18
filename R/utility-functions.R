526dec8479d37b24c00972f4efd861d44e7783b5
h.bengtsson <h.bengtsson@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-01-18 19:47:21 +0000

    Clarified error message
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/codetoolsBioC@98442 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utility-functions.R
+++ R/utility-functions.R
@@ -1,7 +1,7 @@
 getPackageEnvironment <- function(package) {
     pname <- paste("package", package, sep = ":")
     if (! pname %in% search())
-        stop(package, " must be loaded")
+        stop(package, " must be loaded and attached")
     if (package %in% loadedNamespaces())
         getNamespace(package)
     else
